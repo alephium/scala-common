578254763e7c496bfa379387e678afcc75f0388a
Cheng Wang <wachmc@gmail.com>
2019-10-06 23:49:23 +0200

    Refactor more

--- rpc/src/main/scala/org/alephium/rpc/model/JsonRPC.scala
+++ rpc/src/main/scala/org/alephium/rpc/model/JsonRPC.scala
@@ -5,8 +5,12 @@ import scala.concurrent.Future
 import com.typesafe.scalalogging.StrictLogging
 import io.circe._
 import io.circe.generic.semiauto._
+import io.circe.syntax._
 
-/* Ref: https://www.jsonrpc.org/specification */
+/* Ref: https://www.jsonrpc.org/specification
+ *
+ * The only difference is that the type for response we use here is Option[Long]
+ */
 
 object JsonRPC extends StrictLogging {
   type Handler = Map[String, Request => Future[Response]]
@@ -46,7 +50,6 @@ object JsonRPC extends StrictLogging {
       }
     }
   }
-
   object RequestUnsafe {
     implicit val decoder: Decoder[RequestUnsafe] = deriveDecoder[RequestUnsafe]
   }
@@ -71,16 +74,20 @@ object JsonRPC extends StrictLogging {
 
   sealed trait Response
   object Response {
-    import io.circe.syntax._
-    import io.circe.generic.auto._
-
     def successful[T <: WithId](request: T): Response               = Success(request.id, Json.True)
     def successful[T <: WithId](request: T, result: Json): Response = Success(request.id, result)
     def failed[T <: WithId](request: T, error: Error): Response     = Failure(Some(request.id), error)
     def failed(error: Error): Response                              = Failure(None, error)
 
-    case class Success(id: Long, result: Json)         extends Response
+    case class Success(id: Long, result: Json) extends Response
+    object Success {
+      implicit val encoder: Encoder[Success] = deriveEncoder[Success]
+    }
     case class Failure(id: Option[Long], error: Error) extends Response
+    object Failure {
+      import io.circe.generic.auto._ // Note: I hate this!
+      implicit val encoder: Encoder[Failure] = deriveEncoder[Failure]
+    }
 
     implicit val encoder: Encoder[Response] = {
       val product: Encoder[Response] = Encoder.instance {
