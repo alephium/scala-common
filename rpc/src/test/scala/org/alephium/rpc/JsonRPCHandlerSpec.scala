3fab6f5997e60cd4b317929a0432ad4e79aaa3f6
Alois Cochard <alois.cochard@gmail.com>
2020-02-04 09:30:03 +0100

    Add `JsonRPCHandlerSpec`.

---
+++ rpc/src/test/scala/org/alephium/rpc/JsonRPCHandlerSpec.scala
@@ -0,0 +1,41 @@
+package org.alephium.rpc
+
+import scala.concurrent.Future
+
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import io.circe.Json
+import io.circe.syntax._
+import io.circe.parser._
+import org.scalatest.EitherValues
+
+import org.alephium.rpc.model._
+import org.alephium.util.AlephiumSpec
+
+class JsonRPCHandlerSpec extends AlephiumSpec with ScalatestRouteTest with EitherValues {
+
+  it should "route http" in {
+    val response = JsonRPC.Response.Success(Json.fromInt(42), 1)
+
+    val handler = Map("foo" -> { _: JsonRPC.Request =>
+      Future.successful(response)
+    })
+
+    val route = JsonRPCHandler.routeHttp(handler)
+
+    val jsonRequest = JsonRPC.Request("foo", None, 1).asJson.noSpaces
+    val httpRequest = HttpRequest(HttpMethods.POST, "/",entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+
+    httpRequest ~> route ~> check {
+      status.intValue is 200
+      val text    = responseAs[String]
+      val json    = parse(text).right.value
+      val success = json.as[JsonRPC.Response.Success].right.value
+
+      text is """{"jsonrpc":"2.0","result":42,"id":1}"""
+      success is response
+    }
+  }
+
+}
+
