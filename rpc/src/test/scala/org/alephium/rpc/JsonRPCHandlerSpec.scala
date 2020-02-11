950b469b9181ee02308663876cb7a2bc9cb1a180
Cheng Wang <wachmc@gmail.com>
2020-02-11 21:40:44 +0100

    Misc updates

--- rpc/src/test/scala/org/alephium/rpc/JsonRPCHandlerSpec.scala
+++ rpc/src/test/scala/org/alephium/rpc/JsonRPCHandlerSpec.scala
@@ -5,15 +5,14 @@ import scala.concurrent.Future
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import io.circe.Json
-import io.circe.syntax._
 import io.circe.parser._
+import io.circe.syntax._
 import org.scalatest.EitherValues
 
 import org.alephium.rpc.model._
 import org.alephium.util.AlephiumSpec
 
 class JsonRPCHandlerSpec extends AlephiumSpec with ScalatestRouteTest with EitherValues {
-
   it should "route http" in {
     val response = JsonRPC.Response.Success(Json.fromInt(42), 1)
 
@@ -24,10 +23,13 @@ class JsonRPCHandlerSpec extends AlephiumSpec with ScalatestRouteTest with Eithe
     val route = JsonRPCHandler.routeHttp(handler)
 
     val jsonRequest = JsonRPC.Request("foo", None, 1).asJson.noSpaces
-    val httpRequest = HttpRequest(HttpMethods.POST, "/",entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+    val httpRequest = HttpRequest(HttpMethods.POST,
+                                  "/",
+                                  entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
 
     httpRequest ~> route ~> check {
       status.intValue is 200
+      contentType is MediaTypes.`application/json`
       val text    = responseAs[String]
       val json    = parse(text).right.value
       val success = json.as[JsonRPC.Response.Success].right.value
@@ -36,6 +38,4 @@ class JsonRPCHandlerSpec extends AlephiumSpec with ScalatestRouteTest with Eithe
       success is response
     }
   }
-
 }
-
