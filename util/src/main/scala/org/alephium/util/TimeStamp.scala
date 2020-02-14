578e8d4e5bef5b312a1d9c2905a5f2f72d8e6457
Cheng Wang <wachmc@gmail.com>
2020-02-14 18:40:46 +0100

    Update Timestamp and Duration

--- util/src/main/scala/org/alephium/util/TimeStamp.scala
+++ util/src/main/scala/org/alephium/util/TimeStamp.scala
@@ -2,25 +2,40 @@ package org.alephium.util
 
 class TimeStamp(val millis: Long) extends AnyVal with Ordered[TimeStamp] {
   def plusMillis(millisToAdd: Long): Option[TimeStamp] =
-    TimeStamp.from(millis + millisToAdd)
+    TimeStamp.ofMillis(millis + millisToAdd)
+
+  def plusMillisUnsafe(millisToAdd: Long): TimeStamp = {
+    TimeStamp.ofMillisUnsafe(millis + millisToAdd)
+  }
 
   def plusSeconds(secondsToAdd: Long): Option[TimeStamp] =
-    TimeStamp.from(millis + secondsToAdd * 1000)
+    TimeStamp.ofMillis(millis + secondsToAdd * 1000)
+
+  def plusSecondsUnsafe(secondsToAdd: Long): TimeStamp = {
+    TimeStamp.ofMillisUnsafe(millis + secondsToAdd * 1000)
+  }
 
   def plusMinutes(minutesToAdd: Long): Option[TimeStamp] =
-    TimeStamp.from(millis + minutesToAdd * 60 * 1000)
+    TimeStamp.ofMillis(millis + minutesToAdd * 60 * 1000)
+
+  def plusMinutesUnsafe(minutesToAdd: Long): TimeStamp = {
+    TimeStamp.ofMillisUnsafe(millis + minutesToAdd * 60 * 1000)
+  }
 
   def plusHours(hoursToAdd: Long): Option[TimeStamp] =
-    TimeStamp.from(millis + hoursToAdd * 60 * 60 * 1000)
+    TimeStamp.ofMillis(millis + hoursToAdd * 60 * 60 * 1000)
 
-  def +(duration: Duration): Option[TimeStamp] =
-    TimeStamp.from(millis + duration.millis)
+  def plusHoursUnsafe(hoursToAdd: Long): TimeStamp =
+    TimeStamp.ofMillisUnsafe(millis + hoursToAdd * 60 * 60 * 1000)
+
+  def +(duration: Duration): TimeStamp =
+    TimeStamp.ofMillisUnsafe(millis + duration.millis)
 
   def -(duration: Duration): Option[TimeStamp] =
-    TimeStamp.from(millis - duration.millis)
+    TimeStamp.ofMillis(millis - duration.millis)
 
-  def diff(another: TimeStamp): Duration =
-    Duration.ofMillis(millis - another.millis)
+  def --(another: TimeStamp): Option[Duration] =
+    Duration.from(millis - another.millis)
 
   def isBefore(another: TimeStamp): Boolean =
     millis < another.millis
@@ -29,16 +44,16 @@ class TimeStamp(val millis: Long) extends AnyVal with Ordered[TimeStamp] {
 }
 
 object TimeStamp {
-  val zero: TimeStamp = unsafeFrom(0)
-
-  def now(): TimeStamp = unsafeFrom(System.currentTimeMillis())
-
-  def unsafeFrom(millis: Long): TimeStamp = {
+  def ofMillisUnsafe(millis: Long): TimeStamp = {
     assume(millis >= 0, "timestamp should be non-negative")
     new TimeStamp(millis)
   }
 
-  def from(millis: Long): Option[TimeStamp] = {
-    if (millis < 0) None else Some(new TimeStamp(millis))
+  def ofMillis(millis: Long): Option[TimeStamp] = {
+    if (millis >= 0) Some(new TimeStamp(millis)) else None
   }
+
+  val zero: TimeStamp = ofMillisUnsafe(0)
+
+  def now(): TimeStamp = ofMillisUnsafe(System.currentTimeMillis())
 }
