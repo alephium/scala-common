5a6a0c897843cce0b07bfca166a3ba1458d8cc40
Cheng Wang <wachmc@gmail.com>
2020-02-14 18:40:46 +0100

    Update Timestamp and Duration

--- util/src/test/scala/org/alephium/util/DurationSpec.scala
+++ util/src/test/scala/org/alephium/util/DurationSpec.scala
@@ -15,45 +15,52 @@ class DurationSpec extends AlephiumSpec {
 
     if (dt.millis <= Long.MaxValue / 2 && dt.millis >= Long.MaxValue / 2) {
       (dt + dt).millis is 2 * dt.millis
-      (dt * 2).millis is 2 * dt.millis
+      (dt timesUnsafe 2).millis is 2 * dt.millis
     }
-    (dt - dt).millis is 0
-    (dt / 2).millis is dt.millis / 2
+    (dt - dt).get.millis is 0
+    (dt divUnsafe 2).millis is dt.millis / 2
   }
 
   it should "initialize correctly" in {
     Duration.zero.millis is 0
     forAll { millis: Long =>
-      whenever(millis > Long.MinValue) { // otherwise it will fail in jdk8!
+      if (millis >= 0) { // otherwise it will fail in jdk8!
         JDuration.ofMillis(millis).toMillis
-        check(Duration.ofMillis(millis), JDuration.ofMillis(millis))
+        check(Duration.ofMillisUnsafe(millis), JDuration.ofMillis(millis))
 
         val seconds = millis / 1000
-        check(Duration.ofSeconds(seconds), JDuration.ofSeconds(seconds))
+        check(Duration.ofSecondsUnsafe(seconds), JDuration.ofSeconds(seconds))
 
         val minutes = seconds / 60
-        check(Duration.ofMinutes(minutes), JDuration.ofMinutes(minutes))
+        check(Duration.ofMinutesUnsafe(minutes), JDuration.ofMinutes(minutes))
 
         val hours = minutes / 60
-        check(Duration.ofHours(hours), JDuration.ofHours(hours))
+        check(Duration.ofHoursUnsafe(hours), JDuration.ofHours(hours))
+      } else {
+        assertThrows[AssertionError](Duration.ofMillisUnsafe(millis))
+        Duration.ofMillis(millis) is None
       }
     }
   }
 
   it should "operate correctly" in {
     forAll { (l0: Long, l1: Long) =>
-      val dt0 = Duration.ofMillis(l0 / 2)
-      val dt1 = Duration.ofMillis(l1 / 2)
-      (dt0 + dt1).millis is dt0.millis + dt1.millis
-      (dt0 - dt1).millis is dt0.millis - dt1.millis
-      (dt0 * 2).millis is dt0.millis * 2
-      (dt0 / 2).millis is dt0.millis / 2
+      whenever(l0 >= 0 && l1 >= 0) {
+        val dt0 = Duration.ofMillisUnsafe(l0 / 2)
+        val dt1 = Duration.ofMillisUnsafe(l1 / 2)
+        (dt0 + dt1).millis is dt0.millis + dt1.millis
+        if (dt0 > dt1) {
+          (dt0 - dt1).get.millis is dt0.millis - dt1.millis
+        }
+        (dt0 timesUnsafe 2).millis is dt0.millis * 2
+        (dt0 divUnsafe 2).millis is dt0.millis / 2
 
-      val maxMS = Long.MaxValue / 1000000
-      if (l0 >= -maxMS && l0 <= maxMS) {
-        Duration.ofMillis(l0).asScala is SDuration.apply(l0, MILLISECONDS)
-      } else {
-        assertThrows[IllegalArgumentException](Duration.ofMillis(l0).asScala)
+        val maxMS = Long.MaxValue / 1000000
+        if (l0 <= maxMS) {
+          Duration.ofMillis(l0).get.asScala is SDuration.apply(l0, MILLISECONDS)
+        } else {
+          assertThrows[IllegalArgumentException](Duration.ofMillis(l0).get.asScala)
+        }
       }
     }
   }
