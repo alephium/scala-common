0688e85439445f04b0709fba009db8a2984cb8c0
Cheng Wang <wachmc@gmail.com>
2020-02-17 19:30:14 +0100

    Add functions for bit operation and AVector

---
+++ util/src/main/scala/org/alephium/util/Bits.scala
@@ -0,0 +1,19 @@
+package org.alephium.util
+
+object Bits {
+  def toPosInt(byte: Byte): Int = {
+    byte & 0xFF
+  }
+
+  def toBytes(value: Int): AVector[Byte] = {
+    AVector((value >> 24).toByte, (value >> 16).toByte, (value >> 8).toByte, value.toByte)
+  }
+
+  def xorByte(value: Int): Byte = {
+    val byte0 = (value >> 24).toByte
+    val byte1 = (value >> 16).toByte
+    val byte2 = (value >> 8).toByte
+    val byte3 = value.toByte
+    (byte0 ^ byte1 ^ byte2 ^ byte3).toByte
+  }
+}
