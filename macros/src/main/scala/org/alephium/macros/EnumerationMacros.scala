a6f3cba03ed1302e69e8b777437e2816a02c23f3
Cheng Wang <wachmc@gmail.com>
2020-03-18 19:06:18 +0100

    Move enumeration macros from util to macros

--- macros/src/main/scala/org/alephium/macros/EnumerationMacros.scala
+++ macros/src/main/scala/org/alephium/macros/EnumerationMacros.scala
@@ -1,4 +1,4 @@
-package org.alephium.util
+package org.alephium.macros
 
 import scala.collection.immutable.TreeSet
 import scala.language.experimental.macros
@@ -13,31 +13,23 @@ object EnumerationMacros {
 
     val symbol = weakTypeOf[A].typeSymbol.asClass
 
-    if (!symbol.isClass || !symbol.isSealed)
+    def sourceModuleRef(sym: Symbol): Ident = {
+      Ident(
+        sym
+          .asInstanceOf[scala.reflect.internal.Symbols#Symbol]
+          .sourceModule
+          .asInstanceOf[Symbol])
+    }
+
+    if (!symbol.isClass || !symbol.isSealed) {
       c.abort(c.enclosingPosition, "Can only enumerate values of a sealed trait or class.")
-    else {
-
+    } else {
       val children = symbol.knownDirectSubclasses.toList
-
       if (!children.forall(_.isModuleClass))
         c.abort(c.enclosingPosition, "All children must be objects.")
       else
         c.Expr[TreeSet[A]] {
-
-          def sourceModuleRef(sym: Symbol) =
-            Ident(
-              sym
-                .asInstanceOf[scala.reflect.internal.Symbols#Symbol]
-                .sourceModule
-                .asInstanceOf[Symbol])
-
-          Apply(
-            Select(
-              reify(TreeSet).tree,
-              TermName("apply")
-            ),
-            children.map(sourceModuleRef(_))
-          )
+          Apply(Select(reify(TreeSet).tree, TermName("apply")), children.map(sourceModuleRef(_)))
         }
     }
   }
