d208fd0494c618daa60739e181db74bb4ee9b25b
Thomas Droxler <thomas.droxler@gmail.com>
2020-03-27 13:36:31 +0100

    Add `Env.Integration`

--- util/src/main/scala/org/alephium/util/Env.scala
+++ util/src/main/scala/org/alephium/util/Env.scala
@@ -5,16 +5,20 @@ sealed trait Env {
 }
 
 object Env {
-  case object Prod  extends Env { override def name: String = "prod" }
-  case object Debug extends Env { override def name: String = "debug" }
-  case object Test  extends Env { override def name: String = "test" }
+  case object Prod        extends Env { override def name: String = "prod" }
+  case object Debug       extends Env { override def name: String = "debug" }
+  case object Test        extends Env { override def name: String = "test" }
+  case object Integration extends Env { override def name: String = "it" }
 
-  def resolve(): Env = {
-    val env = System.getenv("ALEPHIUM_ENV")
+  def resolve(): Env =
+    resolve(System.getenv("ALEPHIUM_ENV"))
+
+  def resolve(env: String): Env = {
     env match {
       case "prod"  => Prod
       case "debug" => Debug
       case "test"  => Test
+      case "it"    => Integration
     }
   }
 }
