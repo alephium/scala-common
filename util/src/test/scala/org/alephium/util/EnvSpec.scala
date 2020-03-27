064089b6261b272011c663fdc39be05524b487e7
Thomas Droxler <thomas.droxler@gmail.com>
2020-03-27 13:36:31 +0100

    Add `Env.Integration`

--- util/src/test/scala/org/alephium/util/EnvSpec.scala
+++ util/src/test/scala/org/alephium/util/EnvSpec.scala
@@ -7,5 +7,15 @@ class EnvSpec extends AlephiumSpec {
     Env.Prod.name is "prod"
     Env.Debug.name is "debug"
     Env.Test.name is "test"
+    Env.Integration.name is "it"
+  }
+
+  it should "resolve correctly" in {
+    Env.resolve("prod") is Env.Prod
+    Env.resolve("debug") is Env.Debug
+    Env.resolve("test") is Env.Test
+    Env.resolve("it") is Env.Integration
+
+    Env.resolve() is Env.Test
   }
 }
