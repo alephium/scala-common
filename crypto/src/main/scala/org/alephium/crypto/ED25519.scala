5335b22c742330a12ddc39a90141eced715082be
Cheng Wang <wachmc@gmail.com>
2020-04-24 11:41:53 +0200

    Use ed25519 from bouncy castle

--- crypto/src/main/scala/org/alephium/crypto/ED25519.scala
+++ crypto/src/main/scala/org/alephium/crypto/ED25519.scala
@@ -1,7 +1,7 @@
 package org.alephium.crypto
 
 import akka.util.ByteString
-import org.whispersystems.curve25519.Curve25519
+import org.bouncycastle.math.ec.rfc8032.{Ed25519 => bcEd25519}
 
 import org.alephium.serde.RandomBytes
 import org.alephium.util.AVector
@@ -10,20 +10,20 @@ class ED25519PrivateKey(val bytes: ByteString) extends PrivateKey
 
 object ED25519PrivateKey
     extends RandomBytes.Companion[ED25519PrivateKey](bs => {
-      assert(bs.size == ed25519KeyLength)
+      assert(bs.size == ed25519PriLength)
       new ED25519PrivateKey(bs)
     }, _.bytes) {
-  override def length: Int = ed25519KeyLength
+  override def length: Int = ed25519PriLength
 }
 
 class ED25519PublicKey(val bytes: ByteString) extends PublicKey
 
 object ED25519PublicKey
     extends RandomBytes.Companion[ED25519PublicKey](bs => {
-      assert(bs.size == ed25519KeyLength)
+      assert(bs.size == ed25519PubLength)
       new ED25519PublicKey(bs)
     }, _.bytes) {
-  override def length: Int = ed25519KeyLength
+  override def length: Int = ed25519PubLength
 }
 
 class ED25519Signature(val bytes: ByteString) extends Signature
@@ -34,28 +34,16 @@ object ED25519Signature
       new ED25519Signature(bs)
     }, _.bytes) {
   override def length: Int = ed25519SigLength
-
-  def isCanonical(bytes: ByteString): Boolean = {
-    assert(bytes.length == length)
-    val sBytes = bytes.takeRight(32).toArray.reverse
-    sBytes(0) = (sBytes(0) & 0x7F).toByte
-    val s = BigInt(sBytes)
-    s >= 0 && s < ED25519.generator
-  }
 }
 
 object ED25519 extends SignatureSchema[ED25519PrivateKey, ED25519PublicKey, ED25519Signature] {
-
-  val curve25519: Curve25519 = Curve25519.getInstance(Curve25519.BEST)
-
-  val generator: BigInt = BigInt(
-    "7237005577332262213973186563042994240857116359379907606001950938285454250989")
-
   override def generatePriPub(): (ED25519PrivateKey, ED25519PublicKey) = {
-    val keyPair    = curve25519.generateKeyPair()
-    val privateKey = ED25519PrivateKey.unsafe(ByteString.fromArrayUnsafe(keyPair.getPrivateKey))
-    val publicKey  = ED25519PublicKey.unsafe(ByteString.fromArrayUnsafe(keyPair.getPublicKey))
-    (privateKey, publicKey)
+    val privateKey = Array.ofDim[Byte](ed25519PriLength)
+    val publicKey  = Array.ofDim[Byte](ed25519PubLength)
+    bcEd25519.generatePrivateKey(RandomBytes.source, privateKey)
+    bcEd25519.generatePublicKey(privateKey, 0, publicKey, 0)
+    (ED25519PrivateKey.unsafe(ByteString.fromArrayUnsafe(privateKey)),
+     ED25519PublicKey.unsafe(ByteString.fromArrayUnsafe(publicKey)))
   }
 
   override def sign(message: ByteString, privateKey: ED25519PrivateKey): ED25519Signature = {
@@ -67,7 +55,8 @@ object ED25519 extends SignatureSchema[ED25519PrivateKey, ED25519PublicKey, ED25
   }
 
   private def sign(message: Array[Byte], privateKey: Array[Byte]): ED25519Signature = {
-    val signature = curve25519.calculateSignature(privateKey, message)
+    val signature = Array.ofDim[Byte](ed25519SigLength)
+    bcEd25519.sign(privateKey, 0, message, 0, message.length, signature, 0)
     ED25519Signature.unsafe(ByteString.fromArrayUnsafe(signature))
   }
 
@@ -86,6 +75,6 @@ object ED25519 extends SignatureSchema[ED25519PrivateKey, ED25519PublicKey, ED25
   private def verify(message: Array[Byte],
                      signature: Array[Byte],
                      publicKey: Array[Byte]): Boolean = {
-    curve25519.verifySignature(publicKey, message, signature)
+    bcEd25519.verify(signature, 0, publicKey, 0, message, 0, message.length)
   }
 }
