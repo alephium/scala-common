df86a7c93da5d17f5eac5c72f17be5149d0acd64
Cheng Wang <wachmc@gmail.com>
2020-06-02 09:51:17 +0200

    Shorten the underlying name for integers

--- util/src/main/scala/org/alephium/util/I256.scala
+++ util/src/main/scala/org/alephium/util/I256.scala
@@ -4,73 +4,73 @@ import java.math.BigInteger
 
 import akka.util.ByteString
 
-class I256(val value: BigInteger) extends AnyVal with Ordered[I256] {
+class I256(val v: BigInteger) extends AnyVal with Ordered[I256] {
   import I256.validate
 
-  @inline def isZero: Boolean = value.signum() == 0
+  @inline def isZero: Boolean = v.signum() == 0
 
   def addUnsafe(that: I256): I256 = {
-    val underlying = this.value.add(that.value)
+    val underlying = this.v.add(that.v)
     assume(validate(underlying))
     I256.unsafe(underlying)
   }
 
   def add(that: I256): Option[I256] = {
-    val underlying = this.value.add(that.value)
+    val underlying = this.v.add(that.v)
     if (validate(underlying)) Some(I256.unsafe(underlying)) else None
   }
 
   def subUnsafe(that: I256): I256 = {
-    val underlying = this.value.subtract(that.value)
+    val underlying = this.v.subtract(that.v)
     assume(validate(underlying))
     I256.unsafe(underlying)
   }
 
   def sub(that: I256): Option[I256] = {
-    val underlying = this.value.subtract(that.value)
+    val underlying = this.v.subtract(that.v)
     if (validate(underlying)) Some(I256.unsafe(underlying)) else None
   }
 
   def mulUnsafe(that: I256): I256 = {
-    val underlying = this.value.multiply(that.value)
+    val underlying = this.v.multiply(that.v)
     assume(validate(underlying))
     I256.unsafe(underlying)
   }
 
   def mul(that: I256): Option[I256] = {
-    val underlying = this.value.multiply(that.value)
+    val underlying = this.v.multiply(that.v)
     if (validate(underlying)) Some(I256.unsafe(underlying)) else None
   }
 
   def divUnsafe(that: I256): I256 = {
     assume(!(that.isZero || (that == I256.NegOne && this == I256.MinValue)))
-    I256.unsafe(this.value.divide(that.value))
+    I256.unsafe(this.v.divide(that.v))
   }
 
   def div(that: I256): Option[I256] = {
     if (that.isZero || (that == I256.NegOne && this == I256.MinValue)) {
       None
     } else {
-      Some(I256.unsafe(this.value.divide(that.value)))
+      Some(I256.unsafe(this.v.divide(that.v)))
     }
   }
 
   def modUnsafe(that: I256): I256 = {
-    assume(!(that.isZero || (this.value == I256.lowerBound && that.value == I256.NegOne.toBigInt)))
-    I256.unsafe(this.value.remainder(that.value))
+    assume(!(that.isZero || (this.v == I256.lowerBound && that.v == I256.NegOne.toBigInt)))
+    I256.unsafe(this.v.remainder(that.v))
   }
 
   def mod(that: I256): Option[I256] = {
-    if (that.isZero || (this.value == I256.lowerBound && that.value == I256.NegOne.toBigInt)) None
-    else Some(I256.unsafe(this.value.remainder(that.value)))
+    if (that.isZero || (this.v == I256.lowerBound && that.v == I256.NegOne.toBigInt)) None
+    else Some(I256.unsafe(this.v.remainder(that.v)))
   }
 
-  def compare(that: I256): Int = this.value.compareTo(that.value)
+  def compare(that: I256): Int = this.v.compareTo(that.v)
 
-  def toBigInt: BigInteger = value
+  def toBigInt: BigInteger = v
 
   def toBytes: ByteString = {
-    val tmp           = ByteString.fromArrayUnsafe(value.toByteArray)
+    val tmp           = ByteString.fromArrayUnsafe(v.toByteArray)
     val paddingLength = 32 - tmp.length
     if (paddingLength > 0) {
       if (this >= I256.Zero) {
